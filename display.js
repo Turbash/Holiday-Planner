c955a84623a46a5c8ce30236ee8ae4485d90df96
Turbash <negirawatdeepi@gmail.com>
2025-06-22 17:16:39 +0530

    Added footer

--- display.js
+++ display.js
@@ -1,7 +1,6 @@
 let card = {};
 
 card.wrap = document.querySelector(".cards");
-// card.new = document.querySelector(".new-card");
 
 card.wrap.addEventListener("click", (e) => {
     if (e.target.classList.contains("card__title")) {
@@ -17,25 +16,3 @@ card.wrap.addEventListener("click", (e) => {
     }
 });
 
-// card.new.addEventListener("click", () => {
-//     let lastCard = card.wrap.querySelector(".card:last-child");
-//     let lastNumber = +lastCard.className.match(/\d+/);
-//     let newCard = lastCard.cloneNode(true);
-//     newCard.className = newCard.className.replace(/\d+/, lastNumber + 1);
-//     newCard.classList.remove("is-active");
-
-//     lastCard.parentElement.insertBefore(newCard, lastCard.nextSibling);
-
-//     reCalcPos();
-// });
-
-// let reCalcPos = () => {
-//     let cards = [].slice.call(card.wrap.querySelectorAll(".card"));
-//     let count = cards.length;
-
-//     cards.forEach((card, i) => {
-//         card.style.transform = `translateY(calc(${
-//             i / count
-//         } * (100% - 1.5rem)))`;
-//     });
-// };
