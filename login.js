71ac06b5761aaeaa234f5c953935865465ea0872
Turbash <negirawatdeepi@gmail.com>
2025-06-22 18:02:32 +0530

    Made the generate form functional Added the generated plan to local storage

--- login.js
+++ login.js
@@ -1,50 +1,50 @@
 const loginTab = document.getElementById('login-tab');
-        const registerTab = document.getElementById('register-tab');
-        const loginForm = document.getElementById('login-form');
-        const registerForm = document.getElementById('register-form');
-        loginTab.onclick = () => {
-            loginTab.classList.add('active');
-            registerTab.classList.remove('active');
-            loginForm.style.display = '';
-            registerForm.style.display = 'none';
-        };
-        registerTab.onclick = () => {
-            registerTab.classList.add('active');
-            loginTab.classList.remove('active');
-            registerForm.style.display = '';
-            loginForm.style.display = 'none';
-        };
+const registerTab = document.getElementById('register-tab');
+const loginForm = document.getElementById('login-form');
+const registerForm = document.getElementById('register-form');
+loginTab.onclick = () => {
+    loginTab.classList.add('active');
+    registerTab.classList.remove('active');
+    loginForm.style.display = '';
+    registerForm.style.display = 'none';
+};
+registerTab.onclick = () => {
+    registerTab.classList.add('active');
+    loginTab.classList.remove('active');
+    registerForm.style.display = '';
+    loginForm.style.display = 'none';
+};
 
-        loginForm.onsubmit = async (e) => {
-            e.preventDefault();
-            const email = loginForm.email.value;
-            const password = loginForm.password.value;
-            const res = await fetch('http://localhost:3000/login', {
-                method: 'POST',
-                credentials: 'include',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ email, password })
-            });
-            const msg = await res.text();
-            console.log(res)
-            console.log(msg)
-            document.getElementById('login-message').textContent = msg;
-            if (res.ok) setTimeout(() => window.location.href = "index.html", 1000);
-        };
+loginForm.onsubmit = async (e) => {
+    e.preventDefault();
+    const email = loginForm.email.value;
+    const password = loginForm.password.value;
+    const res = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password })
+    });
+    const msg = await res.text();
+    console.log(res)
+    console.log(msg)
+    document.getElementById('login-message').textContent = msg;
+    if (res.ok) setTimeout(() => window.location.href = "index.html", 1000);
+};
 
-        registerForm.onsubmit = async (e) => {
-            e.preventDefault();
-            const email = registerForm.email.value;
-            const password = registerForm.password.value;
-            const res = await fetch('http://localhost:3000/register', {
-                method: 'POST',
-                credentials: 'include',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ email, password })
-            });
-            const msg = await res.text();
-            console.log(res);
-            console.log(msg);
-            document.getElementById('register-message').textContent = msg;
-            if (res.ok) setTimeout(() => window.location.href = "index.html", 1000);
-        };
+registerForm.onsubmit = async (e) => {
+    e.preventDefault();
+    const email = registerForm.email.value;
+    const password = registerForm.password.value;
+    const res = await fetch('http://localhost:3000/register', {
+        method: 'POST',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password })
+    });
+    const msg = await res.text();
+    console.log(res);
+    console.log(msg);
+    document.getElementById('register-message').textContent = msg;
+    if (res.ok) setTimeout(() => window.location.href = "index.html", 1000);
+};
---
+++ script.js
@@ -0,0 +1,20 @@
+const form=document.getElementById('survey-form');
+form.onsubmit=async(e)=>{
+    e.preventDefault();
+    const group_type=form.group_type.value;
+    const no_of_days=form.no_of_days.value;
+    const no_of_people=form.no_of_people.value;
+    const budget_in_rupees=form.budget_in_rupees.value;
+    const location=form.location.value;
+    const res=await fetch('http://localhost:3000/generate', {
+        method: 'POST',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ group_type,budget_in_rupees,no_of_people,location,no_of_days })
+    });
+    const plan=await res.text();
+    localStorage.setItem("Plan",plan);
+    if(res.ok){
+        window.location.href="display.html";
+    }
+};
